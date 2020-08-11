e761b69a44483ed3feff8d631db7359f1aae265e
smithspencer817 <smithspencer817@gmail.com>
2020-08-11 13:28:33 -0500

    Done.

---
+++ sushi-saga-client/src/components/LessButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LessButton = (props) => {
+    return <button onClick={props.handleLessSushi}>
+            Less sushi!
+          </button>
+}
+
+export default LessButton
